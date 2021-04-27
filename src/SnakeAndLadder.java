1fa2d7cf12f8e8ed9176794feb171f7b1b5cd39e
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-27 15:09:16 +0530

    Added: position of the player

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,6 +3,9 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
+		
+		int position=0;
+		System.out.println("Position of the player: "+ position);
 
 	}
 
