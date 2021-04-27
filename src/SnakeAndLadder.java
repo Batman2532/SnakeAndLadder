38bd1b8e766972bed215d09133715f561ad5a795
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-27 16:15:48 +0530

    Added: roll a die until win

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,28 +4,44 @@ public class SnakeAndLadder {
 	public static final int  noPlay = 0;
 	public static final int  ladder = 1;
 	public static final int  snake = 2;
-	
+	public static final int  winPosition = 100;
+	public static final int  positionCheck = 0;
     public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
-		
+		int numberOfDie=0;
 		int position = 0;
 		System.out.println("Position of the player: "+ position);
+		
+		while ( position <= winPosition) {
+		if(position<0) {
+			position = 0;
+		}
+		else {
 		int die = (int)(Math.random()*6) + 1;
+		
 		System.out.println("Number on die: "+ die);
 		int ladderSnake = (int)(Math.random()*3) + 1;
+		
 		switch(ladderSnake) {
 		case noPlay:
 			System.out.println("noPlay is come");
 			break;
 		case ladder:
-			position  = position + die;
+			
+			position=position + die;
 			System.out.println("Position of the player: "+ position);
+			numberOfDie++;
 			break;
 		case snake:
-			position = position +die;
+			
+			position=position - die;
+			
 			System.out.println("Position of the player: "+ position);
+			numberOfDie++;
 			break;
 		}
+		}
+		System.out.println("Nummber of Die used: "+ numberOfDie);
 	}
-
+  }
 }
